edb19fd9633d066ee16b8ef44564a617fe611dee
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-08-24 03:13:35 -0400

    Support smart_settings api of fallback to simple getattr technique

--- apps/lock_manager/conf/settings.py
+++ apps/lock_manager/conf/settings.py
@@ -1,5 +1,20 @@
-from django.conf import settings
+"""Configuration options for the django locking app"""
 
 DEFAULT_LOCK_TIMEOUT_VALUE = 10
 
-DEFAULT_LOCK_TIMEOUT = getattr(settings, 'LOCK_MANAGER_DEFAULT_LOCK_TIMEOUT', DEFAULT_LOCK_TIMEOUT_VALUE)
+try:
+    from smart_settings.api import register_settings
+
+    register_settings(
+        namespace=u'lock_manager',
+        module=u'lock_manager.conf.settings',
+        settings=[
+            {'name': u'DEFAULT_LOCK_TIMEOUT', 'global_name': u'LOCK_MANAGER_DEFAULT_LOCK_TIMEOUT', 'default': DEFAULT_LOCK_TIMEOUT_VALUE},
+        ]
+    )
+
+except ImportError:
+    from django.conf import settings
+
+
+    DEFAULT_LOCK_TIMEOUT = getattr(settings, 'LOCK_MANAGER_DEFAULT_LOCK_TIMEOUT', DEFAULT_LOCK_TIMEOUT_VALUE)
